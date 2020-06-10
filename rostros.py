0cee61423eb4cc4be74764c232ddec290a551ce8
goen <jmiguelgonzaleze@gmail.com>
2020-06-09 19:08:36 -0500

    Se agregaron archivos de haar cascade

--- rostros.py
+++ rostros.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python3
 
-import cv2 as cv
+import cv2
 import sys
 
-img = cv.imread("prueba.jpg")
+cascPath = "haarcascade_frontalface_alt2.xml"
+faceCascade = cv2.CascadeClassifier(cascPath)
+image = cv2.imread('prueba.jpg')
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+faces = faceCascade.detectMultiScale(
+    gray,
+    scaleFactor=1.1,
+    minNeighbors=5,
+    minSize=(30, 30),
+    flags = cv2.CASCADE_SCALE_IMAGE
+)
+print("Se encontraron {0} rostros".format(len(faces)))
+for (x, y, w, h) in faces:
+    cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
-if img is None:
-    sys.exit("Could not read the image.")
-
-cv.imshow("Display window", img)
-k = cv.waitKey(0)
-
-if k == ord("s"):
-    cv.imwrite("starry_night.png", img)
+cv2.imshow("Rostros encontrados", image)
+cv2.waitKey(0)
